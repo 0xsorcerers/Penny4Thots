191eea77db6aadb2052269c732d28e3ffb06d4bc
Sandbox user <tools@notanumber.com>
2026-02-01 08:35:55 +0000

    instead of the M placeholder use my image white-on-backgroundas my project logo all through the app. Also let's rename our App to Penny4Thots and reintroduce a welcome page with cool 3D CSS animations...

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThirdwebProvider } from "thirdweb/react";
+import Welcome from "./pages/Welcome";
 import Index from "./pages/Index";
 import MarketPage from "./pages/MarketPage";
 import NotFound from "./pages/NotFound";
@@ -18,7 +19,8 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<Welcome />} />
+            <Route path="/app" element={<Index />} />
             <Route path="/market/:id" element={<MarketPage />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
