a061de633e89aa088b65750e02929188af9ff9dc
0xsorcerers <nuvie.odu@gmail.com>
2026-02-18 09:07:06 +0100

    Fix: Minor Type and lint errors

--- webapp/src/tools/networkData.tsx
+++ webapp/src/tools/networkData.tsx
@@ -1,6 +1,16 @@
 import { type Address } from "viem";
 
-const sepolia = {
+export interface NetworkConfig {
+  name: string;
+  chainId: number;
+  rpc: string;
+  blockExplorer: string;
+  decimals: number;
+  symbol: string;
+  contract_address: Address;
+}
+
+const sepolia: NetworkConfig = {
   name: 'Sepolia (Testnet)',
   chainId: 11155111,
   rpc: 'https://0xrpc.io/sep',
@@ -10,7 +20,7 @@ const sepolia = {
   contract_address: '0x929A04E8d5d8aFBCA5C6cE0e9Fe05f506081cc27' as Address,
 };
 
-const bnb = {
+const bnb: NetworkConfig = {
   name: 'BNB Network (Live)',
   chainId: 56,
   rpc: 'https://bsc-dataseed.binance.org',
@@ -20,7 +30,7 @@ const bnb = {
   contract_address: '0x683a3d9b7723f29eaf2e5511F94F02Dab5f1a633' as Address,
 };
 
-const opbnb = {
+const opbnb: NetworkConfig = {
   name: 'opBNB L2 (Live)',
   chainId: 204,
   rpc: 'https://opbnb-mainnet-rpc.bnbchain.org',
@@ -30,6 +40,6 @@ const opbnb = {
   contract_address: '0x9dc5736Db801272B2357962448B189f5A77a5e36' as Address,
 };
 
-const chains = [sepolia, bnb, opbnb];
+const chains: NetworkConfig[] = [sepolia, bnb, opbnb];
 
-export { chains };
+export { chains, type NetworkConfig };
