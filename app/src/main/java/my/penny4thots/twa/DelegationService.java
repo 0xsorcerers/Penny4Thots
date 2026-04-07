01446c2ed11937991776efebc0a8d548c7c64a05
0xsorcerers <nuvie.odu@gmail.com>
2026-04-07 16:29:10 +0100

    Relegated some networks.

---
+++ app/src/main/java/my/penny4thots/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package my.penny4thots.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
